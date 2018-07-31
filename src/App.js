babd3ea998f0b8b505349c6c4d50f8118a785d46
AA1452 <35006066+AA1452@users.noreply.github.com>
2018-07-31 15:08:24 -0500

    Added centered navbar and background image (first)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, MenuItem, Nav, NavDropdown, NavItem } from 'react-bootstrap';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                {/*<header className="App-header" style={{alignItems: "horizontal"}}>
+                    <h1 className="App-title">AT</h1>
+                </header>*/}
+                <Navbar fluid collapseOnSelect className = "navbar-main">
+                    <Navbar.Header>
+                        <div className = "navbar-brand">
+                            <p className="brand-name">AT</p>
+                        </div>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                        <Nav>
+                            <NavItem eventKey={1} href="#">
+                                Link
+                            </NavItem>
+                            <NavItem eventKey={2} href="#">
+                                Link
+                            </NavItem>
+                            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                                <MenuItem eventKey={3.1}>Action</MenuItem>
+                                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                                <MenuItem divider />
+                                <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                            </NavDropdown>
+                        </Nav>
+                        <Nav pullRight>
+                            <NavItem eventKey={1} href="#">
+                                Link Right
+                            </NavItem>
+                            <NavItem eventKey={2} href="#">
+                                Link Right
+                            </NavItem>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                <div className="homepage">
+                    <img src={require('./assets/ripplesHigh.jpg')} className="background-img"/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
